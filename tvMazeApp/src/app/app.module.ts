29d5ccb2ea8f5b0f3e02464bb23e61c140fa5e8a
slavcho.karanfilovsk <slavco.karafilovski@gmail.com>
2023-04-20 01:32:32 +0200

    added footer

--- tvMazeApp/src/app/app.module.ts
+++ tvMazeApp/src/app/app.module.ts
@@ -8,9 +8,10 @@ import { AppComponent } from './app.component';
 import SearchComponent from './search/search.component';
 import { DetailsComponent } from './details/details.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, SearchComponent, DetailsComponent],
+  declarations: [AppComponent, SearchComponent, DetailsComponent, FooterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
